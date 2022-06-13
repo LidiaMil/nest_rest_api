1ed3da8af9f2786a5fa8d05be67b34e0945ca242
LidiaMil <lidamilos@rambler.ru>
2022-06-13 12:48:00 +0300

    feat: add mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://Lidiia:qwerqwer@cluster0.nnomy.mongodb.net/products?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+})],
   controllers: [AppController],
   providers: [AppService],
 })
